7b8878321ac2f609722de2dd9589e75b5fffcf05
Igor <kravchenkoigor010687@gmail.com>
2023-03-05 10:54:21 +0100

    Save all

--- task0/Program.cs
+++ task0/Program.cs
@@ -8,6 +8,10 @@ Console.Clear();
 
 int[]array = GetArray(10,0,10);
 Console.WriteLine(String.Join(" ",array));
+// ReverseArray1(array);
+// Console.WriteLine(String.Join(" ", array));
+int[] reversArray = ReversArray2(array);
+Console.WriteLine(String.Join(" ",reversArray));
 
 
 
@@ -17,6 +21,27 @@ int[] GetArray(int size, int minValue,int maxValue)
     for(int i = 0; i < size;i++)
     {
         res[i] = new Random().Next(minValue,maxValue+1);
+    }
+    return res;
+}
+
+void ReverseArray1(int[] inArray)
+{   
+    int length = inArray.Length;
+    for (int i = 0; i < inArray.Length / 2; i++)
+    {
+         int k = inArray[i];
+         inArray[i] = inArray[inArray.Length -1 - i];
+         inArray[inArray.Length -1 -i] = k;
+    }
+}
+
+int[] ReversArray2(int[] inArray)
+{
+    int[] res = new int[inArray.Length];
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        res[i] =inArray[inArray.Length -1 -i];
     }
     return res;
 }
